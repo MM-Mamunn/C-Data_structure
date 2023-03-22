6b3f71bb2cb226ec9367473b699e50e334c38517
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2023-03-22 22:15:58 +0600

    Queuee

---
+++ CPP-Data_structure/queuee.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+template <class T>
+class queuee
+{
+public:
+    T arr[100];
+    int n,f =0,r=0;
+
+    queuee(int n)
+    {
+        this-> n =n;
+    }
+    void push(T x)
+    {
+        if(f == 0 && r == n || (r+1) == f)
+        {
+            cout<<"Overflow\n";
+        }
+        else
+        {
+            arr[r] = x;
+            r+=1;
+            if(r > n)
+                r= 0;
+        }
+        cout<<"f r "<<f<<" "<<r<<endl;
+    }
+    void pop()
+    {
+        if(f == r)
+        {
+            cout<<"underflow\n";
+            f=0,r=0;
+        }
+        else
+        {
+            f+=1;
+            if(f> n)
+                f = 0;
+        }
+        cout<<"f r "<<f<<" "<<r<<endl;
+    }
+    void show()
+    {
+        int i =f;
+        while(i != r)
+        {
+            cout<<arr[i]<<" ";
+            i++;
+            if(i == (n+1))
+                i= 0;
+        }
+        cout<<endl;
+    }
+    void top()
+    {
+        cout<<arr[f]<<endl;
+    }
+};
+int main()
+{
+    int k;
+    queuee<int> q(5);
+
+
+    for(;;)
+    {
+        cin>>k;
+        if( k == 1)
+        {
+            int j;
+            cin>>j;
+            q.push(j);
+            q.top();
+        }
+        else
+        {
+            q.pop();
+            q.top();
+        }
+    }
+    return 0;
+}
