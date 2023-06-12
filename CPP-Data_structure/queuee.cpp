0b1ebce2ba3de01f25c7846eaeea8c3b90ca0f41
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2023-06-12 13:04:35 +0600

    Update queuee.cpp

--- CPP-Data_structure/queuee.cpp
+++ CPP-Data_structure/queuee.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 #include <numeric>
 using namespace std;
@@ -16,59 +15,90 @@ public:
     }
     void push(T x)
     {
-        if(f == 0 && r == n || (r+1) == f)
+        if(f == 1 && r == n || (r+1) == f)
         {
             cout<<"Overflow\n";
         }
+        else if(f == 0)
+        {
+            f+=1;
+            r+=1;
+            arr[r] = x;
+        }
         else
         {
-            arr[r] = x;
             r+=1;
             if(r > n)
-                r= 0;
+                r= 1;
+            arr[r] = x;
+
         }
-        cout<<"f r "<<f<<" "<<r<<endl;
+        //cout<<"f r "<<f<<" "<<r<<endl;
     }
     void pop()
     {
-        if(f == r)
+        if(f == 0 )
         {
             cout<<"underflow\n";
             f=0,r=0;
+        }
+        else if(f == r)
+        {
+            f=0;
+            r=0;
         }
         else
         {
             f+=1;
             if(f> n)
-                f = 0;
+                f = 1;
         }
         cout<<"f r "<<f<<" "<<r<<endl;
     }
     int sizee()
-    { if( r<f)
-        return ((n - (f-r)) +1);
-      else
-        return (r-f);
+    {
+        if(f == 0)
+            return 0;
+        else if( r<f)
+            return ((n-f)+r+1);
+        else
+            return (r-f+1);
     }
 
     void show()
     {
         int i =f;
-        while(i != r)
+        if(f == 0)
         {
+            cout<<"Empty\n";
+            return;
+        }
+        else if(f == r)
+        {
+            cout<<arr[f]<<endl;
+            return;
+        }
+        for(;;)
+        {
+            if(i == r)
+            {
+                cout<<arr[i]<<" ";
+                break;
+            }
+
             cout<<arr[i]<<" ";
             i++;
             if(i == (n+1))
-                i= 0;
+                i= 1;
         }
         cout<<endl;
     }
     void top()
     {
-        if(f == r)
+        if(f == 0)
             cout<<"0\n";
         else
-        cout<<arr[f]<<endl;
+            cout<<arr[f]<<endl;
     }
 
 };
@@ -76,37 +106,34 @@ int main()
 {
     int k;
     queuee<int> q(5);
-   q.top();
-
 
     for(;;)
     {
-        cout<<"Enter 1 for push\nEnter 2 for pop\nEnter 3 for size\nEnter 4 for top\n";
+        cout<<"Enter 1 for push\nEnter 2 for pop\nEnter 3 for size\nEnter 4 for top\nEnter 5 for show all\n";
         cin>>k;
         if( k == 1)
         {
             int j;
             cin>>j;
             q.push(j);
-            //cout<<q.sizee()<<endl;
-           q.top();
-
+            q.top();
         }
         else if(k == 2)
         {
             q.pop();
-         //   cout<<q.sizee()<<endl;
-
-            //  q.top();
+        }
+        else if( k == 3)
+        {
+            cout<<q.sizee()<<endl;
         }
         else if( k == 4)
         {
             q.top();
         }
 
-          else if( k == 3)
+        else if( k == 5)
         {
-            cout<<q.sizee()<<endl;
+            q.show();
         }
     }
     return 0;
