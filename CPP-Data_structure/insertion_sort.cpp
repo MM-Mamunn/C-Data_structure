31fde5b48905927dd5e6a9ce2f8d7b7d7bca5b82
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2023-08-24 10:27:05 +0600

    Update insertion_sort.cpp

--- CPP-Data_structure/insertion_sort.cpp
+++ CPP-Data_structure/insertion_sort.cpp
@@ -1,4 +1,43 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int     n;
+    cin>>n;
+    int     a[n+1];
+    
+    for(int i =1; i<=n; i++)
+        cin>>a[i];
+    
+    for(int i =2; i<=n; i++)
+    {
+        int     key = a[i];
+        int     j = i-1;
+        while(j>0 && a[j] > key)
+        {
+            a[j+1] = a[j];
+            j-=1;
+        }
+        a[j+1]= key;
+    }
+    
+    for(int i =1; i<=n; i++)
+        cout<<a[i]<<" ";
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+/* method related to theory as arr[0] = -infinity */
+
+#include<bits/stdc++.h>
 
 using namespace std;
 
